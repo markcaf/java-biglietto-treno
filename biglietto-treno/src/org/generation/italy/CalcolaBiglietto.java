2b3a67673c497bbc3805665ac2aec82e00403f25
Marco Cafasso <marcocafassog@gmail.com>
2022-11-15 14:57:23 +0100

    feat: add scanner for user infos and calculate ticket's price
    Add java.util.scanner to get userKm and userAge, then calculate the ticket's price (0.21 * km)

--- biglietto-treno/src/org/generation/italy/CalcolaBiglietto.java
+++ biglietto-treno/src/org/generation/italy/CalcolaBiglietto.java
@@ -1,9 +1,28 @@
 package org.generation.italy;
 
+import java.util.Scanner;
+
 public class CalcolaBiglietto {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		/*
+		Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero. 
+		Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+		il prezzo del biglietto è definito in base ai km (0.21 € al km)
+		va applicato uno sconto del 20% per i minorenni
+		va applicato uno sconto del 40% per gli over 65 
+		*/
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Inserisci il numero di chilometri da percorrere: ");
+		int userKm = sc.nextInt();
+		
+		System.out.print("Inserisci l'età del passeggero: ");
+		int userAge = sc.nextInt();
+		
+		double ticketPrice = 0.21 * userKm;
+		System.out.println("Il prezzo del biglietto è: " + String.format("%.2f", ticketPrice));
 
 	}
 
